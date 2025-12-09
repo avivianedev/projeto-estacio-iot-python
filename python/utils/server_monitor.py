3decd093025074caaae6f52992506e26fd903752
avivianedev <developer.viviane@gmail.com>
2025-12-09 19:59:00 -0300

    refactor: atualiza arquitetura para o padrão MVC e corrigir tratamento de erros
    - Refactor ServerMonitor to return structured dictionary instead of loose variables
    - Fix: Resolve bug de nan na renderizacao do app

--- python/utils/server_monitor.py
+++ python/utils/server_monitor.py
@@ -1,20 +1,24 @@
+import os
 import requests
 import math
+
 from utils.email_sender import email_sender, check_last_email
-import os
 
 SERVER_EPS32 = os.getenv('SERVER_EPS32')
+MAX_SAFE_TEMP = 26.0
+MIN_SAFE_TEMP = 23.0
 
 class ServerMonitor:
-    def __init__(self, server_ip):
-        self.server_ip = server_ip      
+    def __init__(self, server_url):
+        self.server_url = server_url      
         
     def connection_server(self,):   
         try:                 
-            response = requests.get(self.server_ip)
-            data = response.json()           
-            temperatura = str(data['feeds'][-1]['field1'])                
-            return temperatura             
+            response = requests.get(self.server_url, timeout=5)
+            data = response.json()  
+            temperature = str(data['feeds'][-1]['field1'])     
+            print(f"Conectado ao servidor. Temperatura recebida: {temperature}°C")           
+            return temperature             
                              
         except Exception as e:
             print(f"Erro ao conectar ao servidor: {e}")
@@ -23,22 +27,28 @@ class ServerMonitor:
 
     def fetch_temperature(self):        
         data = self.connection_server()
-        if data:            
-            return float(data)
+        
+        if data is not None:            
+            try:
+                return float(data)
+            except ValueError:
+                return math.nan
         else:
-            return         
+            return math.nan    
     
-    def evaluate_temperature(self,):        
+    def evaluate_temperature(self,):              
         temperature = self.fetch_temperature()
+
         if not math.isnan(temperature):
-            if temperature > 26:
+            
+            if temperature > MAX_SAFE_TEMP:
                 image = "temperatura-alta.png"
                 condition = "Temperatura quente"
                 message = "Recomenda-se resfriar o ambiente." 
                 if check_last_email():               
                     email_sender(temperature)
                 
-            elif temperature < 23:
+            elif temperature < MIN_SAFE_TEMP:
                 image = "frio.png"
                 condition = "Temperatura fria"
                 message = "Recomenda-se aquecer o ambiente"
@@ -52,6 +62,6 @@ class ServerMonitor:
             condition = "Erro"
             message = "Erro ao obter a temperatura."   
 
-        return image, condition, message
+        return image, condition, message, temperature
     
 
